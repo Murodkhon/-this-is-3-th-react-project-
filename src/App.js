e9d6247f1eb2266439033ead8022dafafbac7a2a
Murodkhon <Murodxon_505@mail.ru>
2019-11-27 16:02:38 +0300

    this my 3th react js project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , { Component }from 'react';
+import uuid from "uuid";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css"
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
 
-function App() {
+class App extends Component {
+state = {
+    id: uuid(),
+    item: "",
+    editItem: false,
+    items :[ 
+      {
+        id: 1,
+        title: "Pol artish"
+      },
+      {
+        id:2,
+        title :"Idish tovoq yuvish"
+      }
+    ]
+  };
+
+handleChange = e => {
+  this.setState({
+    item: e.target.value
+  });
+};
+handleSubmit = e => {
+  e.preventDefault();
+
+  const newItem= {
+    id: this.state.id,
+    title: this.state.item
+  };
+  const updatedArray = [...this.state.items, newItem];
+
+  this.setState({
+    item: "",
+    id: uuid(),
+    editItem:false,
+    items:updatedArray
+  });
+
+
+};
+clearList = () =>  {
+  this.setState({
+    items:[]
+  });
+};
+handleEdit = id => {
+  const filteredItems =this.state.items.filter(item => item.id !== id);
+  const selectedItem=this.state.items.find(item => item.id === id);
+  this.setState({
+    items:filteredItems,
+    item: selectedItem.title,
+    id: id,
+    editItem :true
+  });
+
+};
+handleDelete = id => {
+  const filteredItems =this.state.items.filter(item => item.id !== id);
+  this.setState({
+    items:filteredItems
+  });
+};
+
+  
+  render() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <TodoInput 
+          item={this.state.item} 
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+        />
+        <TodoList 
+          items={this.state.items}
+          handleEdit={this.handleEdit}
+          handleDelete={this.handleDelete}
+          clearList={this.clearList}
+        />
+
+      </div>
+      
     </div>
   );
 }
+}
 
 export default App;
+
+// react-router
